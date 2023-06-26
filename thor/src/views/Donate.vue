273e798189fe9a3b9234fecb1575f691d5c44ef2
geekhall <174821300@qq.com>
2023-06-26 17:05:03 +0800

    chore: change pic

--- thor/src/views/Donate.vue
+++ thor/src/views/Donate.vue
@@ -2,7 +2,7 @@
   <div class="content-container">
     <div class="plugins-tips">
       如果该框架对你有帮助，那就请作者喝杯饮料吧！<el-icon><ColdDrink /></el-icon>
-      加微信号yinyang_007探讨问题。
+      加微信号geekhour_net探讨问题。
     </div>
     <div class="wechat-img">
       <img src="../assets/img/wechat.png" />
